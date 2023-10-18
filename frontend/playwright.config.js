b0b6d626335d9185a38e9bc3e82d5e816223efdc
reidfluegel <reid.fluegel@gmail.com>
2023-10-18 10:00:08 -0400

    add frontend skeleton, integrate into CI/CD

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -1,14 +1,16 @@
+const isStaging = process.env.NODE_ENV === 'staging';
+
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
-	webServer: {
-		command: process.env.NODE_ENV === 'staging' ? null : 'npm run build && npm run preview',
-		port: 4173,
-	},
-	use: {
-		baseURL: process.env.NODE_ENV === 'staging' ? 'https://web-app-template.fly.dev/' : 'http://localhost:4173',
-	},
-	testDir: 'tests',
-	testMatch: /(.+\.)?(e2e)\.[jt]s/,
+    webServer: isStaging ? undefined : {
+        command: 'npm run build && npm run preview',
+        port: 4173,
+    },
+    use: {
+        baseURL: isStaging ? 'https://web-app-template.fly.dev/' : 'http://localhost:4173',
+    },
+    testDir: 'tests',
+    testMatch: /(.+\.)?(e2e)\.[jt]s/
 };
 
 export default config;
