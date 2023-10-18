ee4c65bbcffca5dfc6e8634e419321a8aeea886c
reidfluegel <reid.fluegel@gmail.com>
2023-10-18 14:46:31 -0400

    add logout route that clears auth store and returns to login

---
+++ frontend/src/routes/logout/+page.js
@@ -0,0 +1,10 @@
+import {redirect} from '@sveltejs/kit';
+import PocketBase from 'pocketbase';
+
+/** @type {import('./$types').LayoutServerLoad} */
+export function load() {
+    const pb = new PocketBase(process.env.BASE_URL);
+    pb.authStore.clear();
+
+    throw redirect(302, '/login');
+}
---
+++ frontend/tests/logout.e2e.js
@@ -0,0 +1,31 @@
+import {expect, test} from '@playwright/test';
+import PocketBase from "pocketbase";
+
+test.describe('Logout page', () => {
+    test('should log user out and redirect to login', async ({page}) => {
+        // given user is logged in
+        if (process.env.NODE_ENV === 'github') {
+            const pb = new PocketBase('http://127.0.0.1:8090')
+            let email = 'test.user@web-app-template.dev';
+            let password = process.env.TEST_USER_PASSWORD;
+            await pb.collection("users").create({
+                email,
+                password,
+                passwordConfirm: password,
+            });
+        }
+
+        await page.goto('/login');
+
+        await page.fill('input[type="email"]', 'test.user@web-app-template.dev');
+        await page.fill('input[type="password"]', process.env.TEST_USER_PASSWORD);
+
+        await page.click('button[type="submit"]');
+
+        // when
+        await page.goto('/logout');
+
+        // then
+        await expect(page.locator('h1')).toHaveText('Login');
+    });
+})
