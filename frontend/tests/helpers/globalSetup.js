455e8c52674554f107a70d21b8db5725ca700564
reidfluegel <reid.fluegel@gmail.com>
2023-10-19 11:38:47 -0400

    todoList displays only todos createdBy logged in user

--- frontend/tests/helpers/globalSetup.js
+++ frontend/tests/helpers/globalSetup.js
@@ -2,7 +2,11 @@ import PocketBase from "pocketbase";
 export default async () => {
     const createFirstAdmin = async () => {
         const pb = new PocketBase(process.env.BASE_URL)
-        await pb.admins.create({email: process.env.PB_ADMIN_EMAIL, password: process.env.PB_ADMIN_PASSWORD})
+        await pb.admins.create({
+            email: process.env.PB_ADMIN_EMAIL,
+            password: process.env.PB_ADMIN_PASSWORD,
+            passwordConfirm: process.env.PB_ADMIN_PASSWORD
+            })
     }
 
     if (process.env.CREATE_FIRST_ADMIN) {
