84cd46f1c6bcfc585df3f2e3159bf864cda81329
reidfluegel <reid.fluegel@gmail.com>
2023-10-23 14:39:40 -0400

    send email verification to registered user

--- frontend/src/routes/register/+page.svelte
+++ frontend/src/routes/register/+page.svelte
@@ -1,20 +1,21 @@
 <script>
 	import PocketBase from 'pocketbase';
-	import { goto } from '$app/navigation';
 
 	let email = '';
 	let password = '';
 	let passwordConfirm = '';
 	let error = '';
+	let registered = false;
 
-	const pb = new PocketBase(process.env.BASE_URL);
+	export let pb = new PocketBase(process.env.BASE_URL);
 
 	async function handleSubmit() {
 		try {
 			await pb
 				.collection('users')
 				.create({ email: email, password: password, passwordConfirm: passwordConfirm });
-			await goto('/login');
+			await pb.collection('users').requestVerification(email);
+			registered = true;
 		} catch (e) {
 			error = 'There was an error registering your account';
 			throw e;
@@ -24,18 +25,22 @@
 
 <main>
 	<h1>Register</h1>
-	<form on:submit|preventDefault={handleSubmit}>
-		<label for="email">Email:</label>
-		<input type="email" id="email" bind:value={email} required />
+	{#if registered}
+		<p>Please verify your email address</p>
+	{:else}
+		<form on:submit|preventDefault={handleSubmit}>
+			<label for="email">Email:</label>
+			<input type="email" id="email" bind:value={email} required />
 
-		<label for="password">Password:</label>
-		<input type="password" id="password" bind:value={password} required />
+			<label for="password">Password:</label>
+			<input type="password" id="password" bind:value={password} required />
 
-		<label for="passwordConfirm">Confirm Password:</label>
-		<input type="password" id="passwordConfirm" bind:value={passwordConfirm} required />
+			<label for="passwordConfirm">Confirm Password:</label>
+			<input type="password" id="passwordConfirm" bind:value={passwordConfirm} required />
 
-		<button type="submit">Register</button>
-	</form>
+			<button type="submit">Register</button>
+		</form>
+	{/if}
 	{#if error}
 		<div class="error" role="alert">{error}</div>
 	{/if}
--- frontend/tests/register.spec.js
+++ frontend/tests/register.spec.js
@@ -1,26 +1,29 @@
-import { test, expect } from '@playwright/test';
-import { loginTestUser } from './helpers/loginTestUser.js';
+import { expect, test } from '@playwright/test';
 import PocketBase from 'pocketbase';
+import { loginTestUser } from './helpers/loginTestUser.js';
 import { authenticateAsAdmin } from './helpers/authenticateAsAdmin.js';
 
 test.describe('Register Page', () => {
 	const pb = new PocketBase(process.env.BASE_URL);
 
-	test('should register a new user and redirect to login', async ({ page }) => {
+	test('should register a new user and notify user to verify their email', async ({ page }) => {
 		//given
 		let email = `test.user-${Math.random()}@temporary.dev`;
 		await page.goto('/register');
 		await page.fill('input[type="email"]', email);
 		await page.fill('input[id="password"]', 'password12');
 		await page.fill('input[id="passwordConfirm"]', 'password12');
+		const requestPromise = page.waitForRequest('**/request-verification');
 
 		try {
 			// when
 			await page.click('button[type="submit"]');
 			await page.waitForTimeout(1000);
+			const request = await requestPromise;
 
 			// then
-			await expect(page.url()).toBe(`${process.env.BASE_URL}/login`);
+			await expect(request.url()).toMatch(/\/request-verification$/);
+			await expect(page.locator('text="Please verify your email address"')).toBeVisible();
 
 			// when
 			await loginTestUser(page, email, 'password12');
