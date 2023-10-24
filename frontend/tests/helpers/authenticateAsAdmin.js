9a597776f13df36854aca535eb3b49e5cbf3cb19
reidfluegel <reid.fluegel@gmail.com>
2023-10-24 15:33:49 -0400

    remove STAGING_PB_ADMIN_PASSWORD

--- frontend/tests/helpers/authenticateAsAdmin.js
+++ frontend/tests/helpers/authenticateAsAdmin.js
@@ -1,7 +1,3 @@
 export async function authenticateAsAdmin(pb) {
-	const password =
-		process.env.NODE_ENV === 'staging'
-			? process.env.STAGING_PB_ADMIN_PASSWORD
-			: process.env.PB_ADMIN_PASSWORD;
-	await pb.admins.authWithPassword(process.env.PB_ADMIN_EMAIL, password);
+	await pb.admins.authWithPassword(process.env.PB_ADMIN_EMAIL, process.env.PB_ADMIN_PASSWORD);
 }
